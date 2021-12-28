b0904605352a54e4df50cb6408c5d2a02ec7fe73
Diego Quispe <drqc0491@gmail.com>
2021-12-28 18:53:04 -0500

    fix user

--- install/init.sh
+++ install/init.sh
@@ -10,7 +10,7 @@ sudo usermod -aG sudo $shell_user
 # echo $shell_user:$shell_password | sudo chpasswd
 sudo apt install git -y
 echo "Cloning repo"
-cd /home/$USER/
+
 git clone -q https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops/install
 sudo DT_ENV_URL=$DT_ENV_URL DT_API_TOKEN=$DT_API_TOKEN DT_PAAS_TOKEN=$DT_PAAS_TOKEN shell_user=$shell_user shell_password=$shell_password ./perform-2022-hot-aiops/install/setup-0.sh
