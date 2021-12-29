f760da2f792b9df2de18688ca4fa0c460e175ea6
Diego Quispe <drqc0491@gmail.com>
2021-12-28 21:24:18 -0500

    f

--- install/init.sh
+++ install/init.sh
@@ -3,7 +3,7 @@ echo "getting vm ready"
 # PRE SETUP                              #
 #########################################
 
-sudo apt-get -qq update && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 # sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo service ssh restart
 sudo usermod -aG sudo $shell_user
@@ -11,7 +11,6 @@ sudo usermod -aG sudo $shell_user
 sudo apt-get install -y liberror-perl -q
 sudo apt install git -y -q
 echo "Cloning repo"
-
 git clone -q https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops/install
 sudo DT_ENV_URL=$DT_ENV_URL DT_API_TOKEN=$DT_API_TOKEN DT_PAAS_TOKEN=$DT_PAAS_TOKEN shell_user=$shell_user shell_password=$shell_password ./perform-2022-hot-aiops/install/setup-0.sh
