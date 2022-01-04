d06a81102c9644600e4a95a3cc93f6137ac61164
Diego Quispe <drqc0491@gmail.com>
2022-01-04 13:50:24 -0500

    update init

--- install/init.sh
+++ install/init.sh
@@ -2,18 +2,18 @@ echo "getting vm ready"
 #########################################
 # PRE SETUP                              #
 #########################################
-
+echo "UPDATING REPO"
 apt-get update -y 
 apt-get install -y -q git vim jq build-essential software-properties-common default-jdk libasound2 libatk-bridge2.0-0 \
  libatk1.0-0 libc6:amd64 libcairo2 libcups2 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libnss3 libxss1 xdg-utils \
  libminizip-dev libgbm-dev libflac8 apache2-utils
-
+echo "INSTALLING USER"
 sudo usermod -aG sudo $shell_user
 echo $shell_user:$shell_password | sudo chpasswd
-apt-get dist-upgrade
+apt-get dist-upgrade -y -q
 apt-get -f install
 sudo apt install git -y -q
-echo "Cloning repo"
+echo "CLONING REPO"
 git clone -q https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops/install
 sudo DT_ENV_URL=$DT_ENV_URL DT_API_TOKEN=$DT_API_TOKEN DT_PAAS_TOKEN=$DT_PAAS_TOKEN shell_user=$shell_user shell_password=$shell_password ./perform-2022-hot-aiops/install/setup-0.sh
