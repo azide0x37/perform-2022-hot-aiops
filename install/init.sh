0dbd9a66a95271c2e05ac004d88f3373949db8d1
Diego Quispe <drqc0491@gmail.com>
2022-01-09 14:11:20 -0500

    update process

--- install/init.sh
+++ install/init.sh
@@ -3,14 +3,14 @@ echo "getting vm ready"
 # PRE SETUP                              #
 #########################################
 echo "UPDATING REPO"
-apt-get update -y > /dev/null
+apt-get update -y &> /dev/null
 apt-get -q install -y git vim jq build-essential software-properties-common default-jdk libasound2 libatk-bridge2.0-0 \
  libatk1.0-0 libc6:amd64 libcairo2 libcups2 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libnss3 libxss1 xdg-utils \
- libminizip-dev libgbm-dev libflac8 apache2-utils > /dev/null
+ libminizip-dev libgbm-dev libflac8 apache2-utils &> /dev/null
 
-apt-get -q dist-upgrade -y 
+apt-get -q dist-upgrade -y &> /dev/null
 apt-get -q -f install
-sudo apt install -q git -y
+sudo apt install -q git -y &> /dev/null
 echo "CLONING REPO"
 git clone -q https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops/install
