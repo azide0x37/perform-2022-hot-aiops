5f1112fe5acaedfc9683a34f4fd6c3b86b36bb0a
Diego Quispe <drqc0491@gmail.com>
2022-01-13 11:01:51 -0500

    clone specific branch

--- install/init.sh
+++ install/init.sh
@@ -13,7 +13,7 @@ apt-get --qq -f install
 sudo apt install git -y
 echo "###################################################################################################################"
 echo "############################# CLONING REPO ########################################################################"
-git clone -q https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
+git clone -q -b v1.1 --single-branch https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops/install
 #DT_CLUSTER_TOKEN => token with permission to generate other tokens in dynatrace environment
 #DT_ENV_URL => URL for the current dynatrace environment
