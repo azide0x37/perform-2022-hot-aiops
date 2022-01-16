30a5c240f44beab116c41d6b24c117f5fc3e9e2c
Diego Quispe <drqc0491@gmail.com>
2022-01-16 17:40:16 -0500

    fix init

--- install/init.sh
+++ install/init.sh
@@ -13,7 +13,8 @@ apt-get --qq -f install
 sudo apt install git -y
 echo "###################################################################################################################"
 echo "############################# CLONING REPO ########################################################################"
-git clone -q -b v1.1 --single-branch https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
+#git clone -q -b v1.1 --single-branch https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
+git clone -q --single-branch https://github.com/dynatrace-ace/perform-2022-hot-aiops.git
 sudo chmod +x -R ./perform-2022-hot-aiops
 #DT_CLUSTER_TOKEN => token with permission to generate other tokens in dynatrace environment
 #DT_ENV_URL => URL for the current dynatrace environment
