0fd4593c100fa008d6d3c5d58a33a580de071fe4
Diego Quispe <drqc0491@gmail.com>
2022-01-16 18:20:41 -0500

    keptn demo

--- exercises/scripts/keptn_event.sh
+++ exercises/scripts/keptn_event.sh
@@ -1,3 +1,7 @@
 
-
-curl -k http://keptn.34.150.131.131.nip.io/api/v1/event
+curl -s -X POST \
+          "$KEPTN_ENDPOINT/v1/event" \
+   --header "x-token: $KEPTN_API_TOKEN" \
+   --header 'Content-Type: application/cloudevents+json' \
+   --data '{"specversion":"1.0","source":"dynatrace","id":"100","time":"","contenttype":"application/json","type": "sh.keptn.event.production.auto_healing_memory.triggered","data": {"State":"open","ProblemID":"100","PID":"100","ProblemTitle":"Demo degradation","ProblemURL":"demo.problem.com","ProblemDetails":"", "Tags":"demo", "ImpactedEntities":"","ImpactedEntity":"","project":"easytravel","stage":"production","service":"allproblems"}}"' 
+ 
